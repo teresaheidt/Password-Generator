d33a0c57bbc76c9a474365ee8f6affb128ad2cb2
Teresa Heidt <teresaheidt@hotmail.com>
2020-07-15 11:21:37 -0500

    updating index.html with requirements

--- script.js
+++ script.js
@@ -1,9 +1,92 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate")
+
+var numberString = "123456";
+var numberArray = numberString.split("");
+
+
+function getPasswordOptions() {
+  var length = parseInt(prompt("How many characters would you like your password to contain?"));
+  if(isNaN(length) === true) {
+    alert("Click ok to continue.")
+    return;
+  }
+  if(length>128) {
+    alert("Less numbers please!");
+    return;
+  }
+  if(length<8) {
+    alert("More numbers please!");
+    return;
+  }
+  var isNum = confirm("Click ok to confirm numbers.");
+
+
+  var isUpper = prompt("Choose an upper case letter.");
+  if(isNaN(length) === true) {
+    alert("Click ok to continue.")
+    return;
+  }
+  if(isLower) {
+    alert("make it a capital letter!");
+    return;
+  }
+
+  var isLower = prompt("Choose a lower case letter.");
+  if(isNaN(length) === true) {
+    alert("Click ok to continue.");
+    return;
+  }
+  if(isUpper) {
+    alert("Make it lower case!");
+    return;
+  }
+
+  var isSymbol = confirm("Click ok to confirm symbols");
+  if(isNaN(length) === true) {
+    alert("Click ok to continue.");
+    return;
+  }
+  if(length<8 || length>128) {
+    alert("Make it a symbol!");
+    return;
+  }
+  
+  if(isNum === false && isUpper === false && isLower === false && isSymbol ===false) {
+    alert("please choice one char type");
+    return;
+  }
+
+  var passwordOptions = {
+    length,
+    isNum,
+    isUpper,
+    isLower,
+    isSymbol
+  }
+  return passwordOptions;
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var options = getPasswordOptions();
+  console.log(options);
+  var passwordArray = [];
+  var results = [];
+  if(options.isNum === true) {
+    passwordArray = passwordArray.concat(numberArray)
+  }
+// if statements to create array go here
+
+
+for(var i = 0; i < options.length; i++) {
+  var index = Math.floor(Math.random() * passwordArray.length);
+  var digit = passwordArray[index];
+  results.push(digit)
+  console.log(results)
+
+}
+  var password = results.join("")
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -12,3 +95,12 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// function sumUp(a,b) {
+//   return a + b 
+
+// }
+// sumUp(10,2)
+// confirm("how are you?")
+
